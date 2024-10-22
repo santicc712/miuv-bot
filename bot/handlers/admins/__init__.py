d32dcf698b798c5b8e455fcc5f322c6c6985b8e7
smithit <blsmithit@gmail.com>
2024-10-22 14:59:21 +0300

    new plate

--- bot/handlers/admins/__init__.py
+++ bot/handlers/admins/__init__.py
@@ -1,9 +1,10 @@
 from . import admin_menu
 from aiogram import Dispatcher
+from . import broadcast
 
 
 def setup(dp: Dispatcher):
     for module in (
-            admin_menu,
+            admin_menu, broadcast
     ):
         module.setup(dp)
